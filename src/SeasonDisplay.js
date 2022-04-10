dfce0e02cac69b9f933925e2acbbf1b72fade6f2
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-04-10 15:27:19 +0430

    React App

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './SeasonDisplay.css'
+
+const confinge = {
+   summer: {text:'IS HEHR SUMMER',iconName:'sun'},
+   winter:{text:'IS HEHR WINTER',iconName:'snowflake'}
+}
+const getSeason = (lat, month) => {
+   if (month > 2 && month < 9) {
+     return lat > 0 ? 'summer': 'winter'
+   } else {
+      return lat > 0 ? 'winter' : "summer"
+  }
+}
+
+const SeasonDisplay = (props) => {
+   const season = getSeason(props.lat, new Date().getMonth())
+      
+   const { text, iconName } = confinge[season]
+   return (
+      <div className={`season-display ${season}`} >
+         <i className={`icone-left massive ${iconName} icon`} />
+         <h1>{text}</h1>
+         <i className={`icone-right massive ${iconName} icon`} />
+      </div>
+   );
+};
+export default SeasonDisplay;
